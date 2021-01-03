32cd58b96287e3df2d456873b0f5f162a1f29175
Christopher Borchert <cborchertrun@gmail.com>
2021-01-03 11:22:02 +0100

    Merge build and dev server

--- build.js
+++ build.js
@@ -5,6 +5,12 @@ const rimraf = require("rimraf");
 
 const generateContent = require("./generateContent.js");
 
+const args = Object.fromEntries(
+  process.argv.slice(2).map((argument) => argument.split("="))
+);
+const isDev = !!args.DEV;
+const PORT = 3000;
+
 process.on("exit", (code) =>
   console.log("Process exit event with code: ", code)
 );
@@ -20,7 +26,8 @@ try {
 }
 
 generateContent(({ outputPath, pageContent, onSuccess }) => {
-  const finalPath = "./build/" + outputPath;
+  const finalPath = `./build/${outputPath}`;
+
   const outputDirectory = path.dirname(finalPath);
   if (!fs.existsSync(outputDirectory)) {
     fs.mkdirSync(outputDirectory, { recursive: true }, (err) => {
@@ -32,6 +39,26 @@ generateContent(({ outputPath, pageContent, onSuccess }) => {
   fs.writeFile(finalPath, pageContent, (err) => {
     // errors will be caught below
     if (err) throw err;
-    onSuccess(finalPath);
+
+    // log message
+    const logPath = isDev
+      ? `http://localhost:${PORT}/${outputPath}`
+      : finalPath;
+    onSuccess(logPath);
   });
 });
+
+// If the dev flag was given,
+if (isDev) {
+  // init server
+  const express = require("express");
+  const app = express();
+
+  app.use(express.static("build"));
+
+  app.listen(PORT, () => {
+    console.log(
+      `Dev server started, serving static build at http://localhost:${PORT}`
+    );
+  });
+}
