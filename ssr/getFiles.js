66d870eeb66dba79cd0c9ba11621facf4cee91c1
Christopher Borchert <cborchertrun@gmail.com>
2021-01-07 12:37:10 +0100

    Refactor getFiles.js

--- ssr/getFiles.js
+++ ssr/getFiles.js
@@ -4,22 +4,19 @@ const fs = require('fs');
 /**
  * Recursively gets all matching files in a directory
  * @param {string} inDir the directory to recursively search
- * @param {array} ext an array of strings representing the file extensions to accept written in lower case ["md", "svelte"]
+ * @param {array} extensions an array of strings representing the file extensions to accept written in lower case ["md", "svelte"]
  */
-function getFiles(inDir, ext) {
+function getFiles(inDir, extensions) {
   let files = [];
-  if (!ext) return files;
+  if (!extensions) return files;
   fs.readdirSync(inDir).forEach((file) => {
     const absPath = path.join(inDir, file);
     const fileExtension = path.extname(file).toLowerCase().replace('.', '');
     if (fs.statSync(absPath).isDirectory()) {
       // if the given "file" is a directory, get its files and add them to the list
-      files = [...files, ...getFiles(absPath, ext)];
-    } else if (absPath && ext && ext.includes(fileExtension)) {
+      files = [...files, ...getFiles(absPath, extensions)];
+    } else if (absPath && extensions && extensions.includes(fileExtension)) {
       // if the given "file" is a file, and it matches the extension, then add it to the list
-      // TODO: allow for several ext
-      // TODO: Match using regex ?
-      // TODO: lowercase extension ?
       files = [...files, absPath];
     }
   });
