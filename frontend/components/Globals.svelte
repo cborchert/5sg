6d744cd82f06eec1c766a1e387f3a1df5c70705f
Christopher Borchert <cborchertrun@gmail.com>
2021-01-11 07:57:04 +0100

    add favicon, manifest, etc.

--- frontend/components/Globals.svelte
+++ frontend/components/Globals.svelte
@@ -1,16 +1,29 @@
 <svelte:head>
+  <!-- site manifest and favicons-->
+  <link rel="manifest" href="/static/site.webmanifest" />
+  <link rel="icon" type="image/png" href="/static/favicon/favicon.ico" />
+
+  <!-- import global css -->
+  <!-- css reset -->
   <link rel="stylesheet" href="/static/styles/normalize.css" />
+  <!-- global styles -->
   <link rel="stylesheet" href="/static/styles/global.css" />
-  <script src="https://cdn.jsdelivr.net/npm/turbolinks@5.2.0/dist/turbolinks.min.js">
-  </script>
+  <!-- highlight.js theme for highlighting code blocks (for blogs and documentation sites, etc.) -->
   <link
     rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.5.0/styles/solarized-dark.min.css" />
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.5.0/highlight.min.js">
+
+  <!-- import global js -->
+  <!-- turbolinks for giving page navigation a more native feel -->
+  <script defer src="https://cdn.jsdelivr.net/npm/turbolinks@5.2.0/dist/turbolinks.min.js">
+  </script>
+  <!-- highlight.js for highlighting code blocks on page load (for blogs and documentation sites, etc.) -->
+  <script defer src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.5.0/highlight.min.js">
   </script>
 </svelte:head>
 
 <body>
-  <script src="/static/scripts/global.js">
+  <!-- import custom global js -- handles turbolinks and highlight.js -->
+  <script defer src="/static/scripts/global.js">
   </script>
 </body>
