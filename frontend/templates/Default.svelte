4ee1dc1c13bb59e8b4270d71e06aa2d4e83aa59b
Christopher Borchert <cborchertrun@gmail.com>
2021-01-13 10:52:40 +0100

    add sibling nodes

--- frontend/templates/Default.svelte
+++ frontend/templates/Default.svelte
@@ -1,19 +1,19 @@
 <script>
   import Page from '../components/Page.svelte';
-  import {
-    getCategoryNames,
-    getCategorySlug,
-    getTagNames,
-    getTagSlug,
-    tagHome,
-    categoryHome,
-  } from '../../config/blogHelpers';
+  import { getCategoryNames, getCategorySlug, getTagNames, getTagSlug } from '../../config/blogHelpers';
 
+  // the rendered content from the source
   export let htmlContent = '';
+  // the data relative to this content
   export let data = {};
+  // the data relative to all nodes
+  export let nodeData = {};
+
+  // extract site title for use in title tag
   export let siteMetadata = {};
   const { name: siteTitle = '' } = siteMetadata;
 
+  // extract frontmatter for seo and page header/attrution
   const frontmatter = data.frontmatter || {};
   const { title: metaTitle, description: metaDescription } = data.seo || {};
   const { title, date, author } = frontmatter;
@@ -21,8 +21,24 @@
     author ? `by ${author} ` : ''
   }`.trim();
 
+  // additional metadata
   const categories = getCategoryNames(data);
   const tags = getTagNames(data);
+
+  // create sibling pages
+  // TODO: This is expensive and repetitive to to each time.
+  //   This should be generated during post processing.
+  const blogPages = Object.values(nodeData)
+    .filter((node) => node.relPath.startsWith('blog/'))
+    .sort((a, b) => {
+      const dateA = (a.frontmatter && a.frontmatter.date) || '';
+      const dateB = (b.frontmatter && b.frontmatter.date) || '';
+      // newest first
+      return dateA > dateB ? -1 : 1;
+    });
+  const currentIndex = blogPages.findIndex((node) => node.relPath === data.relPath);
+  const prevPost = currentIndex > 0 && blogPages[currentIndex - 1];
+  const nextPost = currentIndex < blogPages.length - 1 && blogPages[currentIndex + 1];
 </script>
 
 <svelte:head>
@@ -58,4 +74,16 @@
   <div>
     {@html htmlContent}
   </div>
+  <footer>
+    <nav>
+      <ul>
+        <li>
+          {#if prevPost}<a href={prevPost.relPath}>← {prevPost.frontmatter.title}</a>{/if}
+        </li>
+        <li>
+          {#if nextPost}<a href={nextPost.relPath}>{nextPost.frontmatter.title} →</a>{/if}
+        </li>
+      </ul>
+    </nav>
+  </footer>
 </Page>
